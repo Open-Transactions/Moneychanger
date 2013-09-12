a9c4bc29b0c40d96647604d367c9edf1ef6d254b
Yamamushi <yamamushi@gmail.com>
2013-09-11 19:51:46 -0500

    Starting to replace UI files with QT Designer created files.

--- src/MarketWindow.cpp
+++ src/MarketWindow.cpp
@@ -1,22 +1,24 @@
-//
-//  MarketWindow.cpp
-//  moneychanger-qt
-//
-//  Created by Jonathan Rumion on 9/11/13.
-//
-//
+#include "marketwindow.h"
+#include "ui_marketwindow.h"
 
-#include "MarketWindow.h"
-
-
-MarketWindow::MarketWindow( QWidget *parent )
-: QDialog( parent )
+MarketWindow::MarketWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::MarketWindow)
 {
-    QPushButton *ok, *cancel;
-    ok = new QPushButton( "OK", this );
-    ok->setGeometry( 10,10, 100,30 );
-    connect( ok, SIGNAL(clicked()), SLOT(accept()) );
-    cancel = new QPushButton( "Cancel", this );
-    cancel->setGeometry( 10,60, 100,30 );
-    connect( cancel, SIGNAL(clicked()), SLOT(reject()) );
+    ui->setupUi(this);
+}
+
+MarketWindow::~MarketWindow()
+{
+    delete ui;
+}
+
+void MarketWindow::on_pushButton_clicked()
+{
+    ui->textEdit->setText("Test Success!");
+}
+
+void MarketWindow::on_pushButton_2_clicked()
+{
+    close();
 }
